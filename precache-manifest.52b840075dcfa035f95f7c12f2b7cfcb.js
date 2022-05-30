fd3483edb53319913ab538f09c18e790832adf7e
Linus Chui <linuschuii@gmail.com>
2022-05-31 01:40:03 +0800

    Updates

---
+++ precache-manifest.52b840075dcfa035f95f7c12f2b7cfcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10595671abbd47e758866b00fb78b8d1",
+    "url": "/where2eat/index.html"
+  },
+  {
+    "revision": "b8a12f37ed88df66a6ad",
+    "url": "/where2eat/static/css/main.48280dd3.chunk.css"
+  },
+  {
+    "revision": "8ea98ab71dfd6b073508",
+    "url": "/where2eat/static/js/2.e226370a.chunk.js"
+  },
+  {
+    "revision": "55b79ebb5e774a9f9242c9bbc135e8e0",
+    "url": "/where2eat/static/js/2.e226370a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a12f37ed88df66a6ad",
+    "url": "/where2eat/static/js/main.f54e709f.chunk.js"
+  },
+  {
+    "revision": "17871c3e38656e2d9566",
+    "url": "/where2eat/static/js/runtime-main.79cd2a4d.js"
+  }
+]);
