ddde3207d15d65213e9f5f3719c867e56e868447
Linus Chui <linuschuii@gmail.com>
2022-06-25 04:47:54 +0800

    added favourites page

--- src/Hero.js
+++ src/Hero.js
@@ -21,7 +21,6 @@ import Footer from './components/Footer';
 import KRNorth from './components/pages/KRNorth';
 import KRSouth from './components/pages/KRSouth';
 import Search from './components/pages/Search';
-import Favourites from './components/pages/Favourites';
 import Favourites2 from './components/pages/Favourites2';
 
 function App2({handleLogOut}) {
@@ -48,7 +47,6 @@ function App2({handleLogOut}) {
           <Route path='/KRNorth' component={KRNorth} />
           <Route path='/KRSouth' component={KRSouth} />
           <Route path='/search' component={Search} />
-          <Route path='/favourites' component={Favourites} />
           <Route path='/favourites2' component={Favourites2} />
         </Switch>
         <Footer />
